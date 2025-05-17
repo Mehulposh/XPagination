abb26bf430f611b131fcd9c24a6ceed240e8b729
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-05-17 15:14:47 +0530

    removed alert if condition

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -8,6 +8,6 @@ export default async function getEmployeeData(){
         return response.data;
     } catch (error) {
         console.error("Error fetching employee data:", error);
-        if(error.response && error.response.status === 500) alert("failed to fetch data");
+        alert("failed to fetch data");
     }
 }
